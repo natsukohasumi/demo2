63ea05d640de98c14181ab2ef624e235191ecce4
hasuminatsuko <natsuko.hasumi@rakus-partners.co.jp>
2025-01-27 15:08:01 +0900

    first commit

---
+++ src/main/java/com/example/demo2/controller/EachController.java
@@ -0,0 +1,30 @@
+package com.example.demo2.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo2.domain.Item;
+
+@Controller
+@RequestMapping("/each")
+public class EachController {
+
+    @RequestMapping("")
+    public String index(Model model){
+
+        List<Item> itemList = new ArrayList<>();
+        itemList.add(new Item("えんぴつ", 120, "緑"));
+        itemList.add(new Item("パソコン", 198000, "シルバー"));
+        itemList.add(new Item("水筒", 2000, "白"));
+
+        model.addAttribute("itemList", itemList);
+
+        return "each";
+
+    }
+
+}
